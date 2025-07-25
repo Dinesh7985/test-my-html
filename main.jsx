62ed9d8d89e0649ca51410c019aa1370e46ee7ad
Dinesh7985 <dineshrajinikanth81@gmail.com>
2025-07-25 09:43:20 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import store from './Redux/store';
-import { Provider } from 'react-redux';
+//import store from './Redux/store';
+//import { Provider } from 'react-redux';
 
 
 createRoot(document.getElementById('root')).render(
   //   <StrictMode>
   //   <App />
   // </StrictMode>,
-   <Provider store={store}>
+  // <Provider store={store}>
     <App />
-  </Provider>,
+ // </Provider>,
 )
